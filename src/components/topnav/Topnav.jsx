3783d3fa4fea1cc4ffaae1eca8bb64b5571e3790
josem109 <josem109@gmail.com>
2021-12-02 21:03:09 -0400

    a

--- src/components/topnav/Topnav.jsx
+++ src/components/topnav/Topnav.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import './topnav.css';
+import Dropdown from '../dropdown/Dropdown';
+import notifications from '../../assets/JsonData/notification.json';
+
+const renderNotificationItem = (item, index) => (
+  <div className="notification-item" key={index}>
+    <i className={item.icon}></i>
+    <span>{item.content}</span>
+  </div>
+);
 const Topnav = () => {
   return (
     <div className="topnav">
@@ -8,9 +17,23 @@ const Topnav = () => {
         <i className="bx bx-search"></i>
       </div>
       <div className="topnav__right">
-        <div className="topnav__right-item">{/*dropdown here*/}</div>
-        <div className="topnav__right-item">{/*dropdown here*/}</div>
-        <div className="topnav__right-item">{/*theme setting*/}</div>
+        <div className="topnav__right-item">
+          <Dropdown icon="bx bx-user" />
+          {/*dropdown here*/}
+        </div>
+        <div className="topnav__right-item">
+          <Dropdown
+            icon="bx bx-bell"
+            badge="12"
+            contentData={notifications}
+            renderItem={(item, index) => renderNotificationItem(item, index)}
+          />
+          {/*dropdown here*/}
+        </div>
+        <div className="topnav__right-item">
+          <Dropdown />
+          {/*theme setting*/}
+        </div>
         {/*hola */}
       </div>
     </div>
